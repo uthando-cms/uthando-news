6fb4b45bdf4a198013d4058dc8759ca8b6cd7474
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-08-24 21:55:56 +0100

    admin menu update

--- config/module.config.php
+++ config/module.config.php
@@ -166,28 +166,24 @@ return [
     ],
     'navigation' => [
         'admin' => [
-            'modules' => [
+            'news' => [
+                'label' => 'News',
                 'pages' => [
-                    'news' => [
-                        'label' => 'News',
-                        'pages' => [
-                            'list' => [
-                                'label'     => 'List All News Items',
-                                'action'    => 'index',
-                                'route'     => 'admin/news',
-                                'resource'  => 'menu:admin'
-                            ],
-                            'add' => [
-                                'label'     => 'Add New News Item',
-                                'action'    => 'add',
-                                'route'     => 'admin/news/edit',
-                                'resource'  => 'menu:admin'
-                            ],
-                        ],
+                    'list' => [
+                        'label'     => 'List All News Items',
+                        'action'    => 'index',
                         'route'     => 'admin/news',
                         'resource'  => 'menu:admin'
                     ],
+                    'add' => [
+                        'label'     => 'Add New News Item',
+                        'action'    => 'add',
+                        'route'     => 'admin/news/edit',
+                        'resource'  => 'menu:admin'
+                    ],
                 ],
+                'route'     => 'admin/news',
+                'resource'  => 'menu:admin'
             ],
         ],
     ],
